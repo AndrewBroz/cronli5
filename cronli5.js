857dd87deea641b1b2ba30dbfc39ddbb66e4cf5e
Andrew Broz <andrew.broz@gmail.com>
2016-04-26 09:12:30 -0500

    update package.json, add readme, build, copyright

--- cronli5.js
+++ cronli5.js
@@ -1,57 +1,25 @@
-// Cron Like I'm Five (cronli5): A Cron Pattern to English Interpreter
-// ===================================================================
-//
-// Generate English language descriptions of schedules from cron patterns.
-// Accepts classic (five-part) cron patterns, or extended (six-part) cron
-// patterns, where the first field is assumed to refer to seconds. Accepts the
-// standard allowed values and the following operators: asterisks (`*`), commas
-// (`,`), hyphens (`-`), and slashes (`/`).
-//
-// The project name is a reference to the internet term [explain like I'm five
-// (abbr. ELI5)][eli5], which is a request for a friendly, simplified, and
-// layman-accessible summary of a text that may be hard to understand without
-// some background. When it comes to cron patterns, most people could use a
-// little ELI5ing.
-//
-// `cronli5` was partially inspired by [`prettycron`][prettycron], which itself
-// is based on code from [an older blog post by dunse][hrce]. However, I wasn't
-// fully satisfied with the human readable output, and didn't need the `moment`
-// and `later` dependencies, since I only needed the cron to English output.
-// This library tries to render as many cron patterns in as straightforward
-// English as possible. `cronli5` was written from scratch. It does not
-// share any code with [`prettycron`][prettycron] or with any other project.
-// Any resemblance to other code, working or not, is purely coincidental.
-//
-// `cronli5` is a good library to use if you need to display an English
-// interpretation of a cron pattern. [`prettycron`][prettycron] is a better
-// choice than `cronli5` if you need to express when the next run will happen
-// or need that value as a date.
-//
-// [prettycron]: https://github.com/azza-bazoo/prettycron
-// [hrce]: http://dsysadm.blogspot.com/2012/09/human-readable-cron-expressions-using.html
-// [eli5]: https://www.reddit.com/r/explainlikeimfive/
-//
-//
-// [*The MIT License (MIT)*](https://opensource.org/licenses/MIT)
-// Copyright(c) 2016 Andrew Broz
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
+/**
+ * @license
+ * Copyright (c) 2016 Andrew Broz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
 (function() {
   // Use English number names for the integers zero through ten.
@@ -345,11 +313,11 @@
     };
   }
 
-  // Export in the browser.
+  // Export in the browser (or browser-like environments).
   if (window) {
     window.cronli5 = cronli5;
   }
-  // Export in Node.
+  // Export in Node (or Node-like environments).
   if (process) {
     module.exports = cronli5;
   }
