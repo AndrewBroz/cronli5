850dbe0bbb85ef3f42c89840a62417a9802fb0ae
Andrew Broz <andrew.broz@gmail.com>
2016-04-27 16:13:14 -0500

    more tests

--- test/simple/objects.js
+++ test/simple/objects.js
@@ -4,7 +4,27 @@ var expect = require('chai').expect;
 describe('Simple valid cron-like objects:', function() {
   equal({second: '*'}, 'every second');
 
+  equal({second: '1'}, 'one second past the minute, every minute');
+
+  equal({second: '5'}, 'five seconds past the minute, every minute');
+
+  equal({second: '10'}, 'ten seconds past the minute, every minute');
+
+  equal({second: '15'}, '15 seconds past the minute, every minute');
+
+  equal({second: '30'}, '30 seconds past the minute, every minute');
+
   equal({minute: '*'}, 'every minute');
+
+  equal({minute: '1'}, 'one minute past the hour, every hour');
+
+  equal({minute: '5'}, 'five minutes past the hour, every hour');
+
+  equal({minute: '10'}, 'ten minutes past the hour, every hour');
+
+  equal({minute: '15'}, '15 minutes past the hour, every hour');
+
+  equal({minute: '30'}, '30 minutes past the hour, every hour');
 
   equal({hour: '*'}, 'every hour');
 });
